14a4001a9744eb2ede335b7e9a7bf392f3153424
uyjang <uyjang@inzisoft.com>
2023-08-06 19:08:09 +0900

    전체적인 디자인 추가. 테스트 json data 추가.

---
+++ app/src/main/java/com/uyjang/korbittest/view/compose/KorbitBox.kt
@@ -0,0 +1,51 @@
+package com.uyjang.korbittest.view.compose
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.uyjang.korbittest.view.ui.theme.KorbitTestTheme
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewKorbitBox() {
+    KorbitTestTheme {
+        KorbitBox() {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(text = "Text")
+                Text(text = "Text")
+            }
+        }
+    }
+}
+
+@Composable
+fun KorbitBox(modifier: Modifier = Modifier, content: @Composable (() -> Unit)? = null) {
+    Box(
+        modifier = Modifier
+            .defaultMinSize(10.dp, 10.dp)
+            .padding(15.dp)
+            .border(1.dp, LocalContentColor.current, shape = RoundedCornerShape(5.dp))
+            .then(modifier) // 테두리 검정색으로 지정
+    ) {
+        content?.invoke()
+    }
+}
--- app/src/main/java/com/uyjang/korbittest/view/fragment/MainFragment.kt
+++ app/src/main/java/com/uyjang/korbittest/view/fragment/MainFragment.kt
@@ -1,15 +1,25 @@
 package com.uyjang.korbittest.view.fragment
 
-import android.webkit.WebView
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -18,16 +28,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.viewModels
 import com.uyjang.korbittest.base.BaseFragment
-import com.uyjang.korbittest.view.ui.theme.KorbitTestTheme
+import com.uyjang.korbittest.view.compose.KorbitBox
 import com.uyjang.korbittest.viewModel.MainViewModel
+import com.uyjang.korbittest.viewModel.MarketData
 
 class MainFragment : BaseFragment() {
 
     private val viewModel: MainViewModel by viewModels()
-
     init {
         baseCompose.content = {
             // A surface container using the 'background' color from the theme
@@ -35,20 +45,57 @@ class MainFragment : BaseFragment() {
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colorScheme.background
             ) {
-                TabScreen()
+                Column(modifier = Modifier.fillMaxSize()) {
+                    TopBar()
+                    TabScreen(viewModel.marketDataList)
+                }
             }
         }
     }
 }
 
 @Composable
-fun TabScreen() {
+fun TopBar() {
+    KorbitBox(modifier = Modifier.fillMaxWidth()) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = "Search Icon",
+                modifier = Modifier
+                    .size(45.dp)
+                    .padding(10.dp),
+                tint = LocalContentColor.current
+            )
+            Text(text = "검색")
+        }
+    }
+}
+
+@Composable
+fun TabScreen(marketDataList: List<MarketData>) {
     var tabIndex by remember { mutableStateOf(0) }
 
     val tabs = listOf("마켓", "즐겨찾기")
 
-    Column(modifier = Modifier.fillMaxWidth()) {
-        TabRow(selectedTabIndex = tabIndex) {
+    Column {
+        TabRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 15.dp),
+            selectedTabIndex = tabIndex,
+            contentColor = LocalContentColor.current, // 탭 텍스트 색상
+            indicator = { tabPositions ->
+                // 인디케이터 색상과 위치 설정
+                TabRowDefaults.Indicator(
+                    Modifier.tabIndicatorOffset(tabPositions[tabIndex]),
+                    color = LocalContentColor.current
+                )
+            },
+            divider = {
+                // 인디케이터 밑에 줄 설정
+                Divider(Modifier.height(0.dp))
+            }
+        ) {
             tabs.forEachIndexed { index, title ->
                 Tab(text = { Text(title) },
                     selected = tabIndex == index,
@@ -56,35 +103,42 @@ fun TabScreen() {
                 )
             }
         }
+        Divider(
+            Modifier
+                .fillMaxWidth()
+                .height(1.dp)
+        )
         when (tabIndex) {
-            0 -> HelloWorld(name = "test1") {
-                
-            }
-            1 -> HelloWorld(name = "test2") {
-
-            }
+            0 -> MarketList(marketDataList)
+            1 -> MarketList(marketDataList)
         }
     }
 }
 
 @Composable
-fun HelloWorld(name: String, onClick : () -> Unit) {
+fun MarketList(marketDataList: List<MarketData>) {
+    LazyColumn {
+        items(marketDataList) { marketData ->
+            MarketDataRow(marketData)
+        }
+    }
+}
+
+@Composable
+fun MarketDataRow(marketData: MarketData) {
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier.padding(16.dp)
     ) {
-        Text(text = "Hello $name!")
-        Button(onClick = { onClick() }) {
-            Text(text = "Go to FirstFragment")
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun MainFragmentPreview() {
-    KorbitTestTheme {
-        TabScreen()
+        Text(text = "통화쌍: ${marketData.currencyPair}")
+        Text(text = "최종 체결 시각: ${marketData.timestamp}")
+        Text(text = "최종 체결 가격: ${marketData.last}")
+        Text(text = "시작 가격: ${marketData.open}")
+        Text(text = "매수호가: ${marketData.bid}")
+        Text(text = "매도호가: ${marketData.ask}")
+        Text(text = "최저가: ${marketData.low}")
+        Text(text = "최고가: ${marketData.high}")
+        Text(text = "거래량: ${marketData.volume}")
+        Text(text = "가격 변동: ${marketData.change}")
+        Text(text = "가격 변동 비율: ${marketData.changePercent}")
     }
 }
